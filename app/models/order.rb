9b4aacc3ad900b8740c89de4072951cb35a716ad
Dawsom Mortenson and Mark Helt <mark@>
2017-12-04 10:55:37 -0800

    seed db, faker, model associations

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,4 @@
 class Order < ApplicationRecord
+  belongs_to :account
+  has_many :order_items
 end
--- db/migrate/20171204175640_create_products.rb
+++ db/migrate/20171204175640_create_products.rb
@@ -1,9 +1,9 @@
 class CreateProducts < ActiveRecord::Migration[5.1]
   def change
     create_table :products do |t|
-      t.column :price, :bigdecimal
+      t.column :price, :decimal
       t.column :name, :string
-      
+
       t.timestamps
     end
   end
