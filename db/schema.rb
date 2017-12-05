715a2e557bfc9c58ce60753c4b405fdc157b9681
Dawson Mortenson and Mark Helt <dawson@>
2017-12-05 09:28:56 -0800

    add paperclip and implement faker to seed photos for our product

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171205005445) do
+ActiveRecord::Schema.define(version: 20171205162116) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,10 @@ ActiveRecord::Schema.define(version: 20171205005445) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "photo_file_name"
+    t.string "photo_content_type"
+    t.integer "photo_file_size"
+    t.datetime "photo_updated_at"
   end
 
   create_table "reviews", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,12 @@ Product.destroy_all
 User.destroy_all
 
 50.times do
-  Product.create!(name: Faker::ElderScrolls.dragon,
-                  price: Faker::Number.decimal(2,2))
+  new_prod = Product.new(
+                name: Faker::ElderScrolls.dragon,
+                price: Faker::Number.decimal(2,2)
+  )
+  new_prod.photo = URI.parse(Faker::LoremPixel.image("600x600"))
+  new_prod.save
 end
 
 p "Created #{Product.count} products"
