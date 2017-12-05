a53a449ccff9a216388c596340e03ed36341eeca
Dawson Mortenson and Mark Helt <dawson@>
2017-12-05 13:27:56 -0800

    different photo sizes, users have one account

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,16 @@
 
 User.destroy_all
+Account.destroy_all
 
 User.create!(email: 'test@gmail.com',
           password: 'password',
           admin: true)
+Account.create
 p "Created #{User.count} users"
 
 50.times do
    Product.create!(
-                name: Faker::ElderScrolls.dragon,
+                name: Faker::Commerce.product_name,
                 price: Faker::Number.decimal(2,2),
                 photo: URI.parse(Faker::LoremPixel.image("600x600"))
                 )
