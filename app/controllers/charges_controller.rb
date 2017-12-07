7d9d19d08e419a10fbaebd388123bda8b9448ed3
Dawson Mortenson and Mark Helt <dawson+mark@>
2017-12-06 19:16:48 -0800

    if charged status to pending

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -11,13 +11,14 @@ class ChargesController < ApplicationController
       :source  => params[:stripeToken]
     )
 
-    charge = Stripe::Charge.create(
+    if charge = Stripe::Charge.create(
       :customer    => customer.id,
       :amount      => (@amount * 100).to_i,
       :description => 'Bob\'s Bees LLC',
       :currency    => 'usd'
     )
-
+      current_order.status = "pending"
+      current_order.save
   rescue Stripe::CardError => e
     flash[:alert] = e.message
     redirect_to new_charge_path
